7a3df59d4c3e7fa8471378386c32cf21ae894e20
Aremaki <ad.remaki@gmail.com>
2023-10-16 18:30:25 +0200

    remove checkpoints

--- eds-medic/eds_medic/pipelines/.ipynb_checkpoints/__init__-checkpoint.py
+++
@@ -1,4 +0,0 @@
-from .doses.factory import create_component as create_doses_component
-from .drugs_normalizer import DrugsNormalizer as create_drugs_normalizer_component
-from .frequencies.factory import create_component as create_frequencies_component
-# from .ner.ner import create_component as create_ner_component
